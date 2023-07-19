1e82a2db0c7629876bf5239120eb1cd6aa9b3a98
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-07-19 18:47:37 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col justify-between min-h-screen">
+      <div>Navbar</div>
+      <div>Main</div>
+      <div>Footer</div>
+    </div>
+  );
+}
+
+export default App;
