7c80004cd2c16126e1b51cdacfe022d25964078e
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-07-20 12:33:02 -0400

    api requests implemented and tested

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,19 @@
 import Display from "./Display";
+import Modal from "./Modal";
+import { fetchDeck } from "../utils/api";
+
+export async function mainLoader() {
+  const deck = await fetchDeck();
+  const deckId = deck.deck_id;
+  return { deck, deckId };
+}
 
 function Main() {
   return (
     <main className="flex flex-col md:flex-row md:justify-center md:gap-8">
       <Display name={"Player"} active={true} />
       <Display name={"Dealer"} active={false} />
+      <Modal />
     </main>
   );
 }
---
+++ src/utils/api.jsx
@@ -0,0 +1,31 @@
+const baseUrl = "https://deckofcardsapi.com/api/deck";
+
+export const fetchDeck = async () => {
+  try {
+    const response = await fetch(`${baseUrl}/new/shuffle/?deck_count=6`);
+    let data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const drawCard = async (deckId, num) => {
+  try {
+    const response = await fetch(`${baseUrl}/${deckId}/draw/?count=${num}`);
+    let data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const reshuffleDeck = async (deckId) => {
+  try {
+    const response = await fetch(`${baseUrl}/${deckId}/shuffle/`);
+    let data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
